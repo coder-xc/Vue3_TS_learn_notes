55e399e0a41494370d4dc53615bde0a19470a65c
coder-xc <1440407551@qq.com>
2022-02-20 16:51:47 +0800

    feat: 学习组件：组件拆分、css作用域、父子组件互相传递、商品页面切换案例

---
+++ 03_learn_component/src/03_父组件传递子组件/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <show-message id="abc" class="xc" title="哈哈哈" content="我是哈哈哈"></show-message>
+    <show-message title="呵呵呵" content="我是呵呵呵"></show-message>
+    <show-message :title="title" :content="content"></show-message>
+    <show-message :title="message.title" :content="message.content"></show-message>
+    <show-message v-bind="message"></show-message>
+    
+    <multi-root-element id="aaa"></multi-root-element>
+  </div>
+</template>
+
+<script>
+import ShowMessage from "./ShowMessage.vue";
+import MultiRootElement from "./MultiRootElement.vue";
+
+export default {
+  components: {
+    ShowMessage,
+    MultiRootElement
+  },
+  data() {
+    return {
+      title: "嘻嘻嘻",
+      content: "我是嘻嘻嘻",
+      message: {
+        title: "嘿嘿嘿",
+        content: "我是嘿嘿嘿",
+      },
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
---
+++ 03_learn_component/src/03_父组件传递子组件/ShowMessage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h2 v-bind="$attrs">{{ title }}</h2>
+    <div>{{ content }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  // props: ["title", "content"]
+  inheritAttrs: false,
+  props: {
+    title: String,
+    content: {
+      type: String,
+      required: true,
+      default: "123",
+    },
+    counter: {
+      type: Number,
+    },
+    info: {
+        type: Object,
+        default: {name: "why"}
+    }
+  },
+};
+</script>
+
+<style scoped>
+</style>
