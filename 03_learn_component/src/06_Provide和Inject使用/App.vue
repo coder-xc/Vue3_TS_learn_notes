09578042554a7652e9d0ea50b6a328b9ad77fd86
coder-xc <1440407551@qq.com>
2022-02-20 23:02:31 +0800

    feat: 学习provide和inject

---
+++ 03_learn_component/src/06_Provide和Inject使用/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <home></home>
+    <button @click="addName">+name</button>
+  </div>
+</template>
+
+<script>
+import Home from "./Hoem.vue";
+import { computed } from "vue";
+
+export default {
+  components: {
+    Home,
+  },
+  provide() {
+    return {
+      name: "xc",
+      age: 18,
+      length: computed(() => this.names.length),
+    };
+  },
+  data() {
+    return {
+      names: ["abc", "cba", "nba"],
+    };
+  },
+  methods: {
+    addName() {
+      this.names.push("xc");
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
---
+++ 03_learn_component/src/06_Provide和Inject使用/HomeContent.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>HomeContent: {{ name }} - {{ age }} - {{ length.value }}</div>
+</template>
+
+<script>
+export default {
+  inject: ["name", "age", "length"],
+};
+</script>
+
+<style scoped>
+</style>
